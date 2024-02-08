6719664b72531c5db88de2fb0e8d9196bfa8527d
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-02-08 15:40:23 +0100

    first

---
+++ lunarlander.js
@@ -0,0 +1,49 @@
+let x = 300;
+let y = 250;
+
+background(135,206,235);
+
+fill(0, 128, 0);
+rect(0, 420, 600, 200);
+
+stroke(43, 29, 20);
+fill(88, 57, 39);
+ellipse(280, 460, 350, 70);
+
+fill(101, 100, 101);
+ellipse(115, 450, 40, 30);
+ellipse(100, 460, 35, 27);
+ellipse(110, 470, 30, 35);
+ellipse(130, 480, 32, 30);
+ellipse(450, 455, 45,35);
+ellipse(450, 470, 40,30);
+
+
+fill(255, 182, 193);
+stroke(255, 105, 180);
+strokeWeight(2);
+ellipse(x-30, y-120, 120, 80);
+ellipse(x-30, y-180, 75, 80);
+
+
+fill(255,255,255);
+ellipse(x-20, y-190, 20);
+ellipse(x-40, y-190, 20);
+
+fill(0,0,0);
+stroke(51,171,249);
+strokeWeight(2);
+ellipse(x-20, y-187, 10);
+ellipse(x-40, y-187, 10);
+
+fill(255, 182, 193);
+stroke(255, 105, 180);
+strokeWeight(2);
+ellipse(x-30, y-170, 35, 25);
+ellipse(x-35, y-170, 2, 5);
+ellipse(x-25, y-170, 2, 5);
+rect(x-71, y-215, 20, 30, 5);
+rect(x-11, y-215, 20, 30, 5);
+rect(x-20, y-95, 35, 25, 5);
+rect(x-70, y-95, 35, 25, 5);
+rect(x-70, y-95, 35, 25, 5);
