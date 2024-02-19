b7de28835aef04aeaef37224d608ca3c2c791d30
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-02-19 22:09:32 +0100

    move pig

--- lunarlander.js
+++ lunarlander.js
@@ -5,6 +5,8 @@ let a = 250;
 let c = 200;
 let l = 200;
 let speed = 2;
+let rotationAngle = 10;
+
 
 //clouds
 
@@ -15,57 +17,12 @@ function cloud(c, l) {
     ellipse(c + 140, l + 15, 50, 40);
 }
 
-// background
-
-function draw() {
-background(135,206,235);
-noStroke();    
-cloud(c - 170, 40);
-cloud(c + 20, 200);
-cloud(c +100, 70);
-cloud(c + 140, 90);
-cloud(c + 200, 300);
-cloud(c - 190, 250);
-c = c + 0.5;
-if (c > width) {
-    c = 0; 
-}
-
-
-// grass 
-fill(0, 128, 0);
-rect(0, 420, 600, 200);
-
-// mud
-ellipse();
-stroke(101, 67, 37);
-fill(88, 57, 39);
-ellipse(z-20, a+210, 350, 70);
-
-// stones
-stroke(85,85,85);
-fill(101, 100, 101);
-ellipse(z-185, a+200, 40, 30);
-ellipse(z-200, a+210, 35, 27);
-ellipse(z-190, a+220, 30, 35);
-ellipse(z-170, a+230, 32, 30);
-ellipse(z+150, a+205, 45,35);
-ellipse(z+150, a+220, 40,30);
-
-z = z + speed;
-if (z >= 370 || z <= 270) {
-    speed *= -1;
-}
-
-
+function pig(x, y) {
 //tail 
-push();
 fill(255, 182, 193);
 stroke(255, 105, 180);
 strokeWeight(2);
-rotate(7);
-rect(x-80, -60, 30, 10, 100);
-pop();
+ellipse(x-95, y-130, 20);
 
 // body for pig
 fill(255, 182, 193);
@@ -101,3 +58,59 @@ rect(x-78, y-140, 22, 32, 20);
 rect(x-78, y-140, 22, 32, 20);
 rect(x-5, y-140, 22, 32, 20);
 }
+
+
+// background
+
+function draw() {
+background(135,206,235);
+noStroke();    
+cloud(c - 170, 40);
+cloud(c + 20, 200);
+cloud(c +100, 70);
+cloud(c + 140, 90);
+cloud(c + 200, 300);
+cloud(c - 190, 250);
+c = c + 0.5;
+if (c > 750) {
+    c = -250; 
+}
+
+pig(x,y);
+if (keyCode == DOWN_ARROW) {
+y = y +3;}
+else if (keyCode == UP_ARROW) {
+    y -= 3;
+}
+else if (keyCode === RIGHT_ARROW) {
+    x += 3;
+}
+else if (keyCode === LEFT_ARROW) {
+    x += 3;
+}
+// grass 
+fill(0, 128, 0);
+rect(0, 420, 600, 200);
+
+// mud
+ellipse();
+stroke(101, 67, 37);
+fill(88, 57, 39);
+ellipse(z-20, a+210, 350, 70);
+
+// stones
+stroke(85,85,85);
+fill(101, 100, 101);
+ellipse(z-185, a+200, 40, 30);
+ellipse(z-200, a+210, 35, 27);
+ellipse(z-190, a+220, 30, 35);
+ellipse(z-170, a+230, 32, 30);
+ellipse(z+150, a+205, 45,35);
+ellipse(z+150, a+220, 40,30);
+
+z = z + speed;
+if (z >= 370 || z <= 270) {
+    speed *= -1;
+}
+}
+
