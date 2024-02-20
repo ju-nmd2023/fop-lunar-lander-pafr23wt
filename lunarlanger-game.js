96f592022a7a30168aa181e2a05eb86768fc3aa4
pafr23wt <148805767+pafr23wt@users.noreply.github.com>
2024-02-20 19:51:48 +0100

    gameover

--- lunarlanger-game.js
+++ lunarlanger-game.js
@@ -6,8 +6,9 @@ let c = 200;
 let l = 200;
 let f = 300;
 let r = 250;
-let speed = 2;
+let speed = 5;
 let s = 1.0;
+let gameStarted = false; 
 
 //clouds in background function
 function cloud(c, l) {
@@ -27,7 +28,14 @@ function cloudfly(c, l) {
     ellipse(c + 70, l + 10, 60, 40);
     ellipse(c + 140, l + 15, 50, 40);
 }
-
+// mud function 
+function mud (z,a) {
+    ellipse();
+    stroke(101, 67, 37);
+    fill(88, 57, 39);
+    ellipse(z-20, a+210);
+    }
+    
 function pig(x, y, s) {
     //tail 
     fill(255, 182, 193);
@@ -47,7 +55,7 @@ function pig(x, y, s) {
     ellipse(x-20 * s, y-190 * s, 20 * s);
     ellipse(x-40 * s, y-190 * s, 20 * s);
     
-    // eyeballs 
+    // eyeballs  
     fill(0,0,0);
     stroke(51,171,249);
     strokeWeight(2);
@@ -69,6 +77,49 @@ function pig(x, y, s) {
     rect(x-78 * s, y-140 * s, 22 * s, 32 * s, 20 * s);
     rect(x-5 * s, y-140 * s, 22 * s, 32 * s, 20 * s);
     }
+
+    function deadpig(x, y, s) {
+        //tail 
+        fill(255, 182, 193);
+        stroke(255, 105, 180);
+        strokeWeight(2);
+        ellipse(x-95 * s, y-130 * s, 20 * s);
+        
+        // body for pig
+        fill(255, 182, 193);
+        stroke(255, 105, 180);
+        strokeWeight(2);
+        ellipse(x-30 * s, y-120 * s, 120 * s, 80 * s);
+        ellipse(x-30 * s, y-180 * s, 75 * s, 80 * s);
+        
+        
+        // dead eyes  
+        stroke(0, 0, 0); 
+        line(x - 25 * s, y - 195 * s, x - 15 * s, y - 185 * s); 
+        line(x - 25 * s, y - 185 * s, x - 15 * s, y - 195 * s); 
+        line(x - 45 * s, y - 195 * s, x - 35 * s, y - 185 * s); 
+        line(x - 45 * s, y - 185 * s, x - 35 * s, y - 195 * s); 
+    
+        // legs and arms
+        fill(255, 182, 193);
+        stroke(255, 105, 180);
+        strokeWeight(2);
+        ellipse(x-30 * s, y-170 * s, 35 * s, 25 * s);
+        ellipse(x-35 * s, y-170 * s, 2 * s, 5 * s);
+        ellipse(x-25 * s, y-170 * s, 2 * s, 5 * s);
+        rect(x-71 * s, y-215 * s, 20 * s, 30 * s, 5 * s);
+        rect(x-11 * s, y-215 * s, 20 * s, 30 * s, 5 * s);
+        rect(x-20 * s, y-95 * s, 32 * s, 22 * s, 5 * s);
+        rect(x-70 * s, y-95 * s, 32 * s, 22 * s, 5 * s);
+        rect(x-78 * s, y-140 * s, 22 * s, 32 * s, 20 * s);
+        rect(x-78 * s, y-140 * s, 22 * s, 32 * s, 20 * s);
+        rect(x-5 * s, y-140 * s, 22 * s, 32 * s, 20 * s);
+        }
+
+    function isPigTouchingMud() {
+        return x - 30 >= z - 175 && x - 30 <= z + 175;
+    }
+
 //flowers on grass function
 function flower (x,y) {
 //steam
@@ -102,28 +153,43 @@ ellipse(x-278, y+198, 12, 12);
     text("Start Game", f + 25, r + 40);
 }
 
+ //try again- button
+ function tryagainbutton(f, r, w, h) {
+    noStroke();
+    fill(0,0,0);
+    textSize(20);
+    text("Sorry, You Loss..", f+ 30, r - 10);
+    noStroke();
+    fill(255, 0, 0);
+    rect(f, r, w, h);
+
+    fill(255, 255, 0);
+    textSize(30);
+    textFont("Georgia");
+    text("Try Again", f + 35, r + 40);
+}
+
+
 function draw() {
-    // If mouse is pressed on the button and game hasn't started yet
     if (!gameStarted && mouseIsPressed && 
         mouseX > f - 100 &&
         mouseX < f - 100 + 200 &&
         mouseY > r - 100 &&
         mouseY < r - 100 + 60) {
         
-        // Set gameStarted to true
         gameStarted = true;
         
-        // Change background
+        // background
         background(135,206,235);
 
-        // Draw grass
+        // grass
         noStroke();
         fill(0, 128, 0);
         rect(0, 420, 600, 200);
 
-        // Draw mud
+        // mud
         fill(88, 57, 39);
-        ellipse(z-20, a+210, 350, 70);
+        ellipse(z-20, a+230, 350, 70);
 
         // Movement for mud
         z = z + speed;
@@ -131,7 +197,7 @@ function draw() {
             speed *= -1;
         }
 
-        // Draw clouds
+        // clouds
         noStroke();    
         cloud(c - 170, 40);
         cloud(c + 20, 200);
@@ -146,7 +212,7 @@ function draw() {
             c = -250; 
         }
 
-        // Draw pig
+        // pig
         pig(x, y, s);
 
         // Movement for pig
@@ -167,8 +233,8 @@ function draw() {
         flower(f, r);
         flower(f + 20, r + 20);
         flower(f + 530, r - 10);
+
     } else if (gameStarted) {
-        // If the game has started, keep rendering the game screen
 
         // Change background
         background(135,206,235);
@@ -180,11 +246,11 @@ function draw() {
 
         // Draw mud
         fill(88, 57, 39);
-        ellipse(z-20, a+210, 350, 70);
+        ellipse(z-20, a+210, 200, 40);
 
         // Movement for mud
         z = z + speed;
-        if (z >= 370 || z <= 270) {
+        if (z >= 440 || z <= 180) {
             speed *= -1;
         }
 
@@ -219,13 +285,27 @@ function draw() {
             x -= 3;
             cloudfly(x - 130, y - 70); 
         }
+        if (keyCode == 32 && isPigTouchingMud()) {
+            background(135,206,235);
+            noStroke();
+            fill(0, 128, 0);
+            rect(0, 350, 600, 300);
+            cloud(c - 170, 40);
+            cloud(c + 20, 200);
+            cloud(c +100, 70);
+            cloud(c + 140, 90);
+            cloud(c + 200, 300);
+            cloud(c - 190, 250);
+            deadpig(x + 30, y + 250 , s * 1.2);
+            tryagainbutton(f - 100, r - 100, 200, 60);
+            }
 
         // Draw flowers
         flower(f, r);
         flower(f + 20, r + 20);
         flower(f + 530, r - 10);
+
     } else {
-        // If the button is not pressed and the game hasn't started, draw the start screen
         background(135,206,235);
         noStroke();
         fill(0, 128, 0);
@@ -243,7 +323,8 @@ function draw() {
         flower(f + 300, r - 15);
         flower(f + 100, r - 15);
         flower(f + 190, r + 30);
-        pig(width / 2, y + 320, Math.max(0.8, (mouseX / width) * 2));
+        pig(width / 2, y + 320, Math.min(2, 2.8 - (mouseY / height) * 2));
         button(f - 100, r - 100, 200, 60);
     }
+
 }
